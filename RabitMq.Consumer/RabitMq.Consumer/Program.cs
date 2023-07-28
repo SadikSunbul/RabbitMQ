5c8714d1eb01f747b48f69d8bcc6f961f6dfa531
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-28 19:44:59 +0300

    RabitMq ilk temel eklendi

---
+++ RabitMq.Consumer/RabitMq.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+
+//Bağlantı oluşturma
+factory.Uri = new("amqps://xomvilok:VrOOuyIz__ROOl7aAzUK6JkwwXlbvpB3@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+//Bağlantı aktifleştirme ve kanal Açma 
+using IConnection connect = factory.CreateConnection();
+
+using IModel chanel = connect.CreateModel();
+
+//Queue Oluşturma
+chanel.QueueDeclare(queue: "example-queue", exclusive: false); //buradakı deger publisher dan kopyalayıp ldık aynı degerlerın olması gerek
+
+//Queue den mesaj okuma 
+
+EventingBasicConsumer consumer = new(chanel); //event tanımlanmalı okuma ıslemı ıcın 
+
+chanel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
+//autoAck:false alınan degerın sılınıp sılınmıycgını ıfade eder
+consumer.Received += (sender, e) =>
+{
+    //Kuyruga gelen mesajın işlendiği yerdir!
+    // e.Body:kuyruktakı mesajın verısını butunsel oolarak getırcektır
+    //e.Body.Span veya e.Body.ToArray() :kuyruktaki mesajın byte verısını getircektir
+
+
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));//byte dizisini strınge dondurduk 
+};
+
+
+Console.Read();
---
+++ RabitMq.Publisher/RabitMq.Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+//Bağlantı olusturma kısmı
+factory.Uri = new("amqps://xomvilok:VrOOuyIz__ROOl7aAzUK6JkwwXlbvpB3@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+
+//Bağlantıyı aktifleştirme ve kanal açma
+
+using IConnection connection = factory.CreateConnection(); //aktıflestırme
+using IModel chanel = connection.CreateModel(); //kanal olusturma 
+
+
+//Queue Oluşturma
+//kanal ustunden kutuk olusturulur
+chanel.QueueDeclare(queue: "example-queue",exclusive:false);
+//exclusive true olursa bu baglantının dısında baska bır baglantı bu kuyrugu kullanamıycaktır false yapmaz ısek consumer kısmından bu kuyruga baglanamayız ve bu kuyruk sılınır 
+
+
+//Queuya mesaj gönderme
+
+//RabbitMQ kuyruga atıcagı mesajları byte türünden kabul etmektedir.Haliyle mesajları bizim byte dönüştürmemiz gerekecektir
+
+//byte[] message=Encoding.UTF8.GetBytes("Merhaba"); //merhaba byta dondururldü 
+
+//chanel.BasicPublish(exchange:"",routingKey: "example-queue",body:message);
+//routingKey: "example-queue" içindeki isimli kuyruga ekle dedik
+//exchange:"" bunu bildirmediğimiz için bu directoryi kabul edecektir ve routingKey e verilen kuyrga ılgılı degerı gonderecektir
+
+for (int i = 0; i < 500; i++)
+{
+    await Task.Delay(100);//bu kadar bekle dedik
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba"+i); //merhaba byta dondururldü 
+
+    chanel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+Console.Read();
