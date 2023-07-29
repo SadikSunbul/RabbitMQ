343aafb4c216a73dbf6ce6b25efa666f68f1def5
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-29 20:18:49 +0300

    Yeni konu eklendi diclera

---
+++ RabitMq.Consumer/DirectExchan.Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xomvilok:VrOOuyIz__ROOl7aAzUK6JkwwXlbvpB3@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+//1.Adım
+chanel.ExchangeDeclare(exchange: "direct-exhange-example", type: ExchangeType.Direct); //bu publısıhrdakı ıle aynı olmalıdır 
+//2.Adım
+string quename = chanel.QueueDeclare().QueueName; //burada kendısı bır kuyruk olusturcak ısmınıde kendisi vericek
+
+//3.Adım
+
+chanel.QueueBind(
+    queue: quename,
+    exchange: "direct-exhange-example",
+    routingKey: "direct-queue-example");
+
+
+EventingBasicConsumer consomer = new(chanel);
+chanel.BasicConsume(
+    queue: quename,
+    autoAck: true,
+    consumer: consomer);
+
+consomer.Received += (send, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
+
+
+//1.Adım: Publisher dakı exchange ile birebir aynı isim ve typ a sahip bır exchange tanımlanmalıdır!
+
+//2.Adım: publisher tarafından routing keyde bulunan degerdekı kuyruga gonderılen mesajları kendı olusturdugumuz kuyruga gonderilen mesajları kendi olusturdugumuz kuyruga yönlendirerek tüketmemiz gerekmektedir. bununn ıcın oncelıkle bır kuyruk olusturulmalıdır 
+
+//3.Adım:
---
+++ RabitMq.Publisher/DirectExchan.Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri =new("amqps://xomvilok:VrOOuyIz__ROOl7aAzUK6JkwwXlbvpB3@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+//DirectExchange
+
+chanel.ExchangeDeclare(exchange: "direct-exhange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.WriteLine("Mesaj:");
+    string mesage = Console.ReadLine();
+    byte[] byteMessage=Encoding.UTF8.GetBytes(mesage);
+
+    chanel.BasicPublish(
+        exchange: "direct-exhange-example", //usteki exchange ısmını aldık 
+        routingKey: "direct-queue-example", //kuruk adı 
+        body:byteMessage);
+}
+
+
+Console.Read();
