6179f99f781c5f581ba225897ebed99bb6d07ec0
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-29 15:48:37 +0300

    Detaylar eklendi

--- RabitMq.Consumer/RabitMq.Consumer/Program.cs
+++ RabitMq.Consumer/RabitMq.Consumer/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 ConnectionFactory factory = new();
 
+//  Consumer
+
 
 //Bağlantı oluşturma
 factory.Uri = new("amqps://xomvilok:VrOOuyIz__ROOl7aAzUK6JkwwXlbvpB3@chimpanzee.rmq.cloudamqp.com/xomvilok");
@@ -15,14 +17,16 @@ using IConnection connect = factory.CreateConnection();
 using IModel chanel = connect.CreateModel();
 
 //Queue Oluşturma
-chanel.QueueDeclare(queue: "example-queue", exclusive: false); //buradakı deger publisher dan kopyalayıp ldık aynı degerlerın olması gerek
+chanel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true); //buradakı deger publisher dan kopyalayıp ldık aynı degerlerın olması gerek
 
 //Queue den mesaj okuma 
 
 EventingBasicConsumer consumer = new(chanel); //event tanımlanmalı okuma ıslemı ıcın 
 
 chanel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
-//autoAck:false alınan degerın sılınıp sılınmıycgını ıfade eder
+//autoAck:false alınan degerın sılınıp sılınmıycgını ıfade eder false deıgımız ıcın consumerdan onay almadıgı surece sılmez degeri
+
+chanel.BasicQos(0,1,false); //burada servise esıt dagılım sevkıyat yapar tutarlı bısekılde esıt tempo ıle calısır
 consumer.Received += (sender, e) =>
 {
     //Kuyruga gelen mesajın işlendiği yerdir!
@@ -31,7 +35,15 @@ consumer.Received += (sender, e) =>
 
 
     Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));//byte dizisini strınge dondurduk 
+
+    //işlemler başarılı tamamlandı ise artık rabbitMQ daki veri silincektir altaki kod ile birlikte
+    chanel.BasicAck(deliveryTag:e.DeliveryTag,multiple:false);
+    //multiple:false sadece bu  mesajı sıler true dersek bundan önceki tum mesajlar sıler bu ıstemedıgımız bır durumdur bu proje ıcın 
+
+    //buradakı kodu yazmaz ısek yenıden baslatırsak burayı silmediği için kuyrukta duruyor veriler ama söyleki default deeğri 30 dk 30 dk sonra kuyruga bu verıler yenıden eklenicek silinmediği için 30 dk sonra bu verıeler yenıden gelicektir buraya
+
 };
 
 
 Console.Read();
+
--- RabitMq.Publisher/RabitMq.Publisher/Program.cs
+++ RabitMq.Publisher/RabitMq.Publisher/Program.cs
@@ -18,7 +18,7 @@ using IModel chanel = connection.CreateModel(); //kanal olusturma
 
 //Queue Oluşturma
 //kanal ustunden kutuk olusturulur
-chanel.QueueDeclare(queue: "example-queue",exclusive:false);
+chanel.QueueDeclare(queue: "example-queue",exclusive:false,durable:true);
 //exclusive true olursa bu baglantının dısında baska bır baglantı bu kuyrugu kullanamıycaktır false yapmaz ısek consumer kısmından bu kuyruga baglanamayız ve bu kuyruk sılınır 
 
 
@@ -32,11 +32,14 @@ chanel.QueueDeclare(queue: "example-queue",exclusive:false);
 //routingKey: "example-queue" içindeki isimli kuyruga ekle dedik
 //exchange:"" bunu bildirmediğimiz için bu directoryi kabul edecektir ve routingKey e verilen kuyrga ılgılı degerı gonderecektir
 
+IBasicProperties properties=chanel.CreateBasicProperties();
+properties.Persistent = true;
+
 for (int i = 0; i < 500; i++)
 {
     await Task.Delay(100);//bu kadar bekle dedik
     byte[] message = Encoding.UTF8.GetBytes("Merhaba"+i); //merhaba byta dondururldü 
 
-    chanel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+    chanel.BasicPublish(exchange: "", routingKey: "example-queue", body: message,basicProperties: properties);
 }
 Console.Read();
