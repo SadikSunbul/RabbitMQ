d16f998bbc593aaa2d0e7f0ff3d4109332828066
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-30 15:10:14 +0300

    P2P tasarımı eklendi

---
+++ RabitMq.Consumer/Message.Tamplate.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xomvilok:lq4WO_gs4cNCDbQY6jh_1u7O8Up8Muxe@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+#region P2P (Point-To-Point) Tasarımı
+//string queueName = "example-p2p-queue";
+//chanel.QueueDeclare( //kurugu olusturduk
+//    queue: queueName,
+//     durable: false,
+//     exclusive: false);
+
+//EventingBasicConsumer consumer = new(chanel);
+//chanel.BasicConsume(
+//    queue: queueName,
+//    autoAck: false,
+//    consumer: consumer);
+//consumer.Received += (send, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+#endregion
+
+#region Publish/Subscrite(Pub/Sub)Tasarımı
+
+#endregion
+#region Work Queue(iş kuyrugu)Tasarımı
+
+#endregion
+#region Request/Response Tasarımı
+
+#endregion
+
+Console.ReadLine();
---
+++ RabitMq.Publisher/Message.Tamplate.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xomvilok:lq4WO_gs4cNCDbQY6jh_1u7O8Up8Muxe@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+#region P2P (Point-To-Point) Tasarımı
+//string queueName = "example-p2p-queue";
+//chanel.QueueDeclare( //kurugu olusturduk
+//    queue: queueName,
+//     durable: false,
+//     exclusive: false);
+
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//chanel.BasicPublish(
+//    exchange: string.Empty, //burası bos ıse buranın default değeri Direct dir
+//    routingKey: queueName,
+//    body: message
+//    );
+#endregion
+
+
+
+
+
+#region Publish/Subscrite(Pub/Sub)Tasarımı
+
+#endregion
+#region Work Queue(iş kuyrugu)Tasarımı
+
+#endregion
+#region Request/Response Tasarımı
+
+#endregion
+
+Console.ReadLine();
