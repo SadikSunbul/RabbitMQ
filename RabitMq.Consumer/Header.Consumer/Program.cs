0d811def05cd06647a14b98aa2ecf2684976b92b
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-30 14:18:24 +0300

    Header Eklendi

---
+++ RabitMq.Consumer/Header.Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xomvilok:66RBb7gvXkydDqRrCN01h48xkNHXCyxW@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers);
+
+Console.Write("Lutfenn header valuesini giriniz:");
+string value = Console.ReadLine();
+
+string queuName = chanel.QueueDeclare().QueueName;
+
+chanel.QueueBind(
+     queue: queuName,
+     exchange: "header-exchange-example",
+     routingKey: string.Empty,
+      new Dictionary<string, object>
+      {
+          ["x-match"]="all",//default tu any dir any dediğimizde sadece bu anahtarı içerenlere gönderiri
+          ["no"] = value
+      }
+    );
+
+EventingBasicConsumer consumer = new(chanel);
+
+chanel.BasicConsume(
+    queue: queuName,
+     autoAck: true,
+     consumer: consumer
+    );
+
+consumer.Received += (send, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+
+Console.Read();
---
+++ RabitMq.Publisher/Header.Publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xomvilok:66RBb7gvXkydDqRrCN01h48xkNHXCyxW@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers);
+
+while (true)
+{
+    Console.Write("Message : ");
+    string mesg = Console.ReadLine();
+    byte[] message = Encoding.UTF8.GetBytes(mesg);
+    Console.WriteLine("Lutfen Header value sını gırını:");
+    string value=Console.ReadLine();
+
+    IBasicProperties basicProperties = chanel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string,object>
+    {
+        ["no"]=value
+    };
+
+    chanel.BasicPublish(
+         exchange: "header-exchange-example",
+         routingKey:string.Empty,
+         body:message,
+         basicProperties: basicProperties
+        );
+}
+
+
+
+Console.Read();
