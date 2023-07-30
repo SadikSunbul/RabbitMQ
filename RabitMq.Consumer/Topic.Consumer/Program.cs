a6a3f55bf16795e4bd388dcdbded25d2d9c16292
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-30 13:02:15 +0300

    Topic eklendi

--- RabitMq.Consumer/Topic.Consumer/Program.cs
+++ RabitMq.Consumer/Topic.Consumer/Program.cs
@@ -1,1 +1,42 @@
 ﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xomvilok:OnlQLdjDE8_P5vs1MqVOyUMJKemAEEno@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(
+    exchange: "topic-excahnge-example",
+    type: ExchangeType.Topic);
+
+Console.Write("Dinlenecek Topic i belirtin:");
+string topi = Console.ReadLine();
+string namequeue = chanel.QueueDeclare().QueueName;
+
+chanel.QueueBind(
+    queue: namequeue,
+     exchange: "topic-excahnge-example",
+      routingKey: topi
+    );
+
+EventingBasicConsumer consumer = new(chanel);
+
+chanel.BasicConsume(
+    queue: namequeue,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (send, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+
+Console.Read();
+
--- RabitMq.Publisher/Topic.Publisher/Program.cs
+++ RabitMq.Publisher/Topic.Publisher/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xomvilok:OnlQLdjDE8_P5vs1MqVOyUMJKemAEEno@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+
+chanel.ExchangeDeclare(
+    exchange: "topic-excahnge-example",
+    type: ExchangeType.Topic);
+
+
+while (true)
+{
+    Console.Write("Mesaj");
+    string ms = Console.ReadLine();
+    byte[] message = Encoding.UTF8.GetBytes(ms);
+    Console.Write("Topic giriniz:");
+    string topic = Console.ReadLine();
+    chanel.BasicPublish(
+        exchange: "topic-excahnge-example",
+        routingKey: topic, //routıng key exchange turune gore rol alamaktadır yanı topıc oluyor burası
+        body: message
+        );
+}
+
+
+Console.Read();
