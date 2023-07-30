0458904d9b56fae83dd6b76c90967186e2b0cf0d
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-30 12:00:33 +0300

    Fanaout eklendi

---
+++ RabitMq.Consumer/Fanout.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Numerics;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new("amqps://xomvilok:i4sdYOrfbvJ0kfPUgouNyiw58U4PXGG6@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(exchange: "fanout-exchane-example", type: ExchangeType.Fanout);
+
+Console.Write("Kuyruk adını giriniz:");
+string _queuName = Console.ReadLine();
+
+chanel.QueueDeclare(queue: _queuName, exclusive: false);//bu isimde bir kuyruk olusturduk
+
+chanel.QueueBind( //exchange ıle kuyrukların ılıskılendırılmesı ıslemı 
+     queue: _queuName,
+      exchange: "fanout-exchane-example",
+      routingKey: string.Empty
+    );
+
+EventingBasicConsumer consumer = new(chanel);
+chanel.BasicConsume(
+    queue: _queuName,
+    autoAck: true,
+     consumer: consumer
+    );
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+
+Console.Read();
---
+++ RabitMq.Publisher/Fanout.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xomvilok:i4sdYOrfbvJ0kfPUgouNyiw58U4PXGG6@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(exchange: "fanout-exchane-example", type: ExchangeType.Fanout);
+
+
+while (true)
+{
+    Console.Write("Mesaj: ");
+    string mesasgea = Console.ReadLine();
+    for (int i = 0; i < 4; i++)
+    {
+        byte[] message = Encoding.UTF8.GetBytes($"{mesasgea} -{i}");
+        chanel.BasicPublish(
+            exchange: "fanout-exchane-example",
+            routingKey: string.Empty, //fanoult ta routıng key onemsız dır cunku tum kuyruklara verıcektır bu degeri
+            body: message
+            );
+    }
+}
+
+
+Console.Read();
